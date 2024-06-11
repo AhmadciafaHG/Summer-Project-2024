49a944711bb941fd420e3c1c99879c02da295b58
AhmadciafaHG <ahmadciafa.allah@hgs.hiddengeniusproject.org>
2024-06-11 12:12:57 -0700

    Added player movement

---
+++ Summer Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float movementSpeed = 10f;
+    public CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>(); // Get the CharacterController component attached to the player
+    }
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal"); // Stores the Horizontal (Left & Right) input of the player
+        float verticalInput = Input.GetAxis("Vertical"); // Stores the Vertical (Forward & Backward) input of the player
+
+        Vector3 movement = new Vector3 (horizontalInput, 0f, verticalInput);
+        movement = transform.TransformDirection (movement);
+        movement *= movementSpeed;
+        controller.Move(movement * Time.deltaTime);
+    }
+}
